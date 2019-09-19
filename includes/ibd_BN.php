51eea1c5e113e25683109b3544d847ee0668d921
Jacek Kowalski <Jacek@jacekk.info>
2019-09-19 11:17:18 +0200

    Zmiana adresu Z39 Biblioteki Narodowej

--- includes/ibd_BN.php
+++ includes/ibd_BN.php
@@ -1,7 +1,7 @@
 <?php
 class ibd_BN extends YAZ_ibd {
 	function __construct() {
-		parent::__construct('Biblioteka Narodowa', 'alpha.bn.org.pl/innopac');
+		parent::__construct('Biblioteka Narodowa', 'eu.alma.exlibrisgroup.com:1921/48OMNIS_NLOP');
 	}
 }
 ?>
